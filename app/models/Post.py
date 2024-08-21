857683e9b24e63d15cce69a5ade9dea4adfd6981
AHuss13 <Adamhuss13@gmail.com>
2024-08-21 17:01:06 -0500

    Updated Post model to include scalar subquery for vote count to clear terminal warning

--- app/models/Post.py
+++ app/models/Post.py
@@ -16,3 +16,4 @@ class Post(Base):
   comments = relationship('Comment', cascade='all,delete')
   votes = relationship('Vote', cascade='all,delete')
   vote_count = column_property(select(func.count(Vote.id)).where(Vote.post_id == id))
+  vote_count = column_property(select(func.count(Vote.id)).where(Vote.post_id == id).scalar_subquery())
